816439d60f88fc34ae59b7c4c2558282e56c4730
Yukee-798 <909397116@qq.com>
2021-03-16 12:31:28 +0800

    init Right

---
+++ src/components/Right/right.tsx
@@ -0,0 +1,13 @@
+import { IBaseProps } from '../../types';
+import './style.scss'
+
+
+const Right: React.FC<IBaseProps> = (props) => {
+    return (
+        <div className='right-container'>
+            right
+        </div>
+    )
+}
+
+export default Right;
