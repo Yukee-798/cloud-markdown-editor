794dd12c1ddd662b440bf672175cfbe78ef5cedd
Yukee-798 <909397116@qq.com>
2021-03-17 12:55:56 +0800

    feature: rename listItem

--- src/components/Left/FileList/fileList.tsx
+++ src/components/Left/FileList/fileList.tsx
@@ -1,59 +1,147 @@
-import { IBaseProps, IFile } from '../../../types';
+import { IBaseProps, IFile, KeyTypes } from '../../../types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { faEdit, faTrashAlt, faCheck } from '@fortawesome/free-solid-svg-icons'
 import { faMarkdown } from '@fortawesome/free-brands-svg-icons'
 
 import './style.scss';
-import { Button, List, Tooltip } from 'antd';
+import { Button, Input, List, Tooltip } from 'antd';
+import { useEffect, useRef, useState } from 'react';
 
 
 
 
 interface IFileListProps extends IBaseProps {
-    files?: any[];
-    onFileClick?: () => void;
-    onFileDelete?: () => void;
-    onSaveEdit?: (id: number, newTitle: string) => void;
+    files?: IFile[];
+    onFileClick?: (id: number) => void;
+    onFileDelete?: (id: number) => void;
+    onTitleEdit?: (id: number, newTitle: string) => void;
+    onFileEdit?: (id: number) => void;
 }
 
 const FileList: React.FC<IFileListProps> = (props) => {
 
 
 
-    const { files } = props;
+    const {
+        files,
+        onFileClick,
+        onFileDelete,
+        onTitleEdit,
+        onFileEdit 
+    } = props;
+
+    const editInputRef = useRef(new Input({defaultValue:''}));
+
+
+    const [isEditTitle, setIsEditTitle] = useState(false);
+    const [fileList, setFileList] = useState<IFile[]>(files as IFile[]);
+    const [editedId, setEditedId] = useState(0);
+
+    /**
+     * 1. 点击编辑按钮后，将 ListItem 的 title 变成一个 Input
+     * 2. Input 的默认值为 item.title 并且聚焦和选中了所有字符串
+     * 3. Input 失去焦点后自动将 Input 的 value 设置给 item.title
+     */
+    const handleEditTitle = (file: IFile) => {
+        setIsEditTitle(true);
+        setEditedId(file.id);
+    }
+
+
+    const handleBlur = () => {
+        setIsEditTitle(false);
+
+        const input = editInputRef.current;
+        const newList: IFile[] = fileList?.map((file) => {
+            if (file.id === editedId) {
+                if (!(input.state.value === file.title)) {
+                    if (window.confirm(`Rename to "${input.state.value}" ?`)) {
+                        return { ...file, title: input.state.value };
+                    }
+                }
+                return file;
+            }
+            return file;
+        });
+        
+        setFileList(newList)
+    }
+
+    useEffect(() => {
+        if (isEditTitle) {
+            const input = editInputRef.current;
+            const value: string = input.state.value;
+            input?.focus();
+            input?.setSelectionRange(0, value.length);
+        }
+    }, [isEditTitle]);
+
+
+    // 如果按下 Esc 则退出编辑状态
+    const cancelEdit = (ev: KeyboardEvent) => {
+        const { key } = ev;
+        if (key === KeyTypes.Esc) {
+            setIsEditTitle(false);
+        }
+    }
+    useEffect(() => {
+        document.addEventListener('keyup', cancelEdit);
+        return () => {
+            document.removeEventListener('keyup', cancelEdit);
+        }
+    })
+
+
     return (
         <div className='fileList-container'>
             <List
                 className='fileList'
-                dataSource={files}
+                dataSource={fileList}
                 renderItem={(item: IFile) => (
-                    <List.Item>
+                    <List.Item
+                        key={item.id}
+                        onClick={() => { onFileClick?.(item.id) }}
+                    >
                         <List.Item.Meta
                             avatar={<FontAwesomeIcon icon={faMarkdown} />}
-                            title={item.title}
+                            title={
+
+
+                                isEditTitle && item.id === editedId ?
+                                    <Input
+                                        ref={editInputRef}
+                                        onBlur={handleBlur}
+                                        defaultValue={item.title}
+                                    /> : item.title
+                            }
                         />
-                        <Tooltip
-                            autoAdjustOverflow
-                            title='edit'
-                        >
-                            <Button
-                                icon={<FontAwesomeIcon icon={faEdit} />}
-                            >
-                            </Button>
 
-                        </Tooltip>
-                        <Tooltip
-                            autoAdjustOverflow
-                            title='delete'
-                        >
-                            <Button
-                                icon={<FontAwesomeIcon icon={faTrashAlt} />}
-                            >
-                            </Button>
-                        </Tooltip>
+                        {
+                            isEditTitle && item.id === editedId ?
+                                <></> :
+                                <>
+                                    <Tooltip
+                                        autoAdjustOverflow
+                                        title='edit'
+                                    >
+                                        <Button
+                                            icon={<FontAwesomeIcon icon={faEdit} />}
+                                            onClick={() => { handleEditTitle(item) }}
+                                        >
+                                        </Button>
 
-
-
+                                    </Tooltip>
+                                    <Tooltip
+                                        autoAdjustOverflow
+                                        title='delete'
+                                    >
+                                        <Button
+                                            icon={<FontAwesomeIcon icon={faTrashAlt} />}
+                                        >
+                                        </Button>
+                                    </Tooltip>
+                                </>
+                        }
                     </List.Item>
                 )}
 
--- src/components/Left/left.tsx
+++ src/components/Left/left.tsx
@@ -1,6 +1,6 @@
 import { Card, Button } from 'antd';
 import FileList from './FileList/fileList'
-
+import { mookFiles } from '../../utils/dev';
 import { IBaseProps } from '../../types';
 import FileSearch from './FileSearch/fileSearch';
 import './style.scss'
@@ -9,149 +9,31 @@ import './style.scss'
 
 
 
-const files: any[] = [
-    {
-        id: 1,
-        title: 'Node.js',
-        body: 'hello world',
-        createAt: 124124125122
-
-    },
-    {
-        id: 2,
-        title: 'React Hooks',
-        body: 'hello !!!',
-        createAt: 12412124128282
-    },
-    {
-        id: 3,
-        title: 'React Hooks',
-        body: 'hello !!!',
-        createAt: 12412124128282
-    },
-    {
-        id: 4,
-        title: 'React Hooks',
-        body: 'hello !!!',
-        createAt: 12412124128282
-    },
-    {
-        id: 5,
-        title: 'React Hooks',
-        body: 'hello !!!',
-        createAt: 12412124128282
-    },
-    {
-        id: 5,
-        title: 'React Hooks',
-        body: 'hello !!!',
-        createAt: 12412124128282
-    },
-    {
-        id: 5,
-        title: 'React Hooks',
-        body: 'hello !!!',
-        createAt: 12412124128282
-    },
-    {
-        id: 5,
-        title: 'React Hooks',
-        body: 'hello !!!',
-        createAt: 12412124128282
-    },
-    {
-        id: 5,
-        title: 'React Hooks',
-        body: 'hello !!!',
-        createAt: 12412124128282
-    },
-    {
-        id: 5,
-        title: 'React Hooks',
-        body: 'hello !!!',
-        createAt: 12412124128282
-    },
-    {
-        id: 5,
-        title: 'React Hooks',
-        body: 'hello !!!',
-        createAt: 12412124128282
-    },
-    {
-        id: 5,
-        title: 'React Hooks',
-        body: 'hello !!!',
-        createAt: 12412124128282
-    },
-    {
-        id: 5,
-        title: 'React Hooks',
-        body: 'hello !!!',
-        createAt: 12412124128282
-    },
-    {
-        id: 5,
-        title: 'React Hooks',
-        body: 'hello !!!',
-        createAt: 12412124128282
-    },
-    {
-        id: 5,
-        title: 'React Hooks',
-        body: 'hello !!!',
-        createAt: 12412124128282
-    },
-    {
-        id: 5,
-        title: 'React Hooks',
-        body: 'hello !!!',
-        createAt: 12412124128282
-    },
-    {
-        id: 5,
-        title: 'React Hooks',
-        body: 'hello !!!',
-        createAt: 12412124128282
-    },
-    {
-        id: 5,
-        title: 'React Hooks',
-        body: 'hello !!!',
-        createAt: 12412124128282
-    },
-    {
-        id: 5,
-        title: 'React Hooks',
-        body: 'hello !!!',
-        createAt: 12412124128282
-    },
-    {
-        id: 5,
-        title: 'React Hooks',
-        body: 'hello !!!',
-        createAt: 12412124128282
-    },
-    {
-        id: 5,
-        title: 'React Hooks',
-        body: 'hello !!!',
-        createAt: 12412124128282
-    },
-    {
-        id: 5,
-        title: 'React Hooks',
-        body: 'hello !!!',
-        createAt: 12412124128282
-    },
-]
+const files = mookFiles(5);
 
 const Left: React.FC<IBaseProps> = (props) => {
 
     const onKeySearch = () => {
 
     }
-    
+
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+    }
+
+    const onFileClick = () => {
+
+    }
+
+    const onFileDelete = () => {
+
+    }
+
+    const onFileEdit = () => {
+
+    }
+
+    const onTitleEdit = () => {
 
     }
 
@@ -159,7 +41,7 @@ const Left: React.FC<IBaseProps> = (props) => {
         <div className='left-container'>
             <Card
                 title={
-                    <FileSearch 
+                    <FileSearch
                         title='Cloud Document'
                         onKeySearch={onKeySearch}
                         // onChange={onChange}
@@ -167,15 +49,18 @@ const Left: React.FC<IBaseProps> = (props) => {
                     />
                 }
             >
-                <FileList 
+                <FileList
                     files={files}
-
+                    onFileClick={onFileClick}
+                    onFileDelete={onFileDelete}
+                    onTitleEdit={onTitleEdit}
+                    onFileEdit={onFileEdit}
                 />
             </Card>
 
             <div className='left-footer'>
-                <Button>123</Button>
-                <Button>123</Button>
+                <Button>New</Button>
+                <Button>Import</Button>
             </div>
 
         </div>
