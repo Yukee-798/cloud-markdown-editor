f4e3a947f7092b479e2d61aefeb7d03f2077e962
Yukee-798 <909397116@qq.com>
2021-03-18 17:33:41 +0800

    update mock

--- src/utils/dev/index.ts
+++ src/utils/dev/index.ts
@@ -1,14 +1,28 @@
-import { IFile } from "../../types";
+import { IFile, ITab } from "../../types";
 
-export const mookFiles = (num: number) => {
+export const mockFiles = (num: number) => {
     let files: IFile[] = [];
-    for (let i = num; i > 0; i--) {
+    for (let i = 0; i < num; i++) {
         files.push({
-            id: i,
-            title: 'Node.js',
-            body: 'hello world',
-            createAt: 124124125122
+            id: i + '',
+            title: 'Node.js' + i,
+            body: 'hello world' + i,
+            createAt: 124124125122 + i
         })
     }
     return files;
+}
+
+export const mockTabs = (num: number) => {
+    let tabs: ITab[] = [];
+    for (let i = 0; i < num; i++) {
+        tabs.push({
+            id: i + '',
+            title: 'Node.js' + i,
+            body: 'hello world' + i,
+            createAt: 124124125122 + i,
+            tabStatus: 0
+        })
+    }
+    return tabs;
 }
