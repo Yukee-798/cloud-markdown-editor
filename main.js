df4180f69ac1c5efdcde0d87a7ab2bd36562ff84
Yukee-798 <909397116@qq.com>
2021-03-19 22:40:04 +0800

    Update config devtools

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-const {BrowserWindow, app} = require('electron');
-const isDev = require('electron-is-dev');
+const {
+    default: installExtension,
+    REDUX_DEVTOOLS
+} = require('electron-devtools-installer');
+
+const isDev = require('electron-is-dev')
+const {app, BrowserWindow} = require('electron');
 
 app.on('ready', () => {
+
+    installExtension(REDUX_DEVTOOLS)
+        .then((name) => console.log(`Added Extension:  ${name}`))
+        .catch((err) => console.log('An error occurred: ', err));
+
     let mainWindow = new BrowserWindow({
         width: 1024,
         height: 680,
@@ -11,8 +21,7 @@ app.on('ready', () => {
             enableRemoteModule: true
         }
     });
+
     const urlLocation = isDev ? 'http://localhost:3000' : 'dummyurl';
     mainWindow.loadURL(urlLocation);
-
 })
-
