005b2fca5036439ca2f01ef6adfa3f3651e6c86d
Yukee-798 <909397116@qq.com>
2021-03-20 20:25:37 +0800

    Update mock data

--- src/utils/dev/index.ts
+++ src/utils/dev/index.ts
@@ -1,15 +1,41 @@
 import { IFile, ITab } from "../../types";
 
 export const mockFiles = (num: number) => {
-    let files: IFile[] = [];
-    for (let i = 0; i < num; i++) {
-        files.push({
-            id: i + '',
-            title: 'Node.js' + i,
-            body: 'hello world hello world hello worldhello world hello world hello world hello world' + i,
-            createAt: 124124125122 + i
-        })
-    }
+    let files: IFile[] = [
+        {
+            id: '0',
+            body: '123',
+            title: 'Node.js',
+            createAt: 1232142124124
+        },
+        {
+            id: '1',
+            body: '456',
+            title: 'Promise',
+            createAt: 1232142124124
+        },
+        {
+            id: '2',
+            body: '789',
+            title: 'Node',
+            createAt: 1232142124124
+        },
+        {
+            id: '3',
+            body: '0',
+            title: 'webpack',
+            createAt: 1232142124124
+        }
+    ];
+    // for (let i = 0; i < num; i++) {
+    //     files.push({
+    //         id: i + '',
+    //         title: 'Node.js' + i,
+    //         body: 'hello world hello world hello worldhello world hello world hello world hello world' + i,
+    //         createAt: 124124125122 + i
+    //     })
+    // }
+    
     return files;
 }
 
