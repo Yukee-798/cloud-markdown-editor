0e3f57275c74a052ac6a45aa879baa0d1cb2be0a
Yukee-798 <909397116@qq.com>
2021-04-07 22:36:05 +0800

    change files path

--- src/utils/hooks/useKeyPress.ts
+++ src/utils/hooks/useKeyPress.ts
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
-import { KeyTypes } from "../types";
-import { judgeSetEqual } from "../utils";
+import { useEffect, useRef, useState } from "react";
+import { KeyTypes } from "../../types";
+import { judgeSetEqual } from "..";
 
 
 
@@ -11,44 +11,30 @@ import { judgeSetEqual } from "../utils";
  */
 const useKeyPress = (targetKeys: KeyTypes[]): boolean => {
 
-    // 数组去重
-    targetKeys = Array.from(new Set([...targetKeys]));
 
     // 实现思路
     // 1. keydown 的时候将往 pressedKeys 里面装 KeyTypes 类型的按键
     // 2. keyup 的时候就从 pressedKeys 弹出对应的按键
     const [isPressed, setIsPressed] = useState(false);
-    const [pressedKeys, setPressedKeys] = useState<KeyTypes[]>([]);
+    const pressedKeysRef = useRef<Set<string>>(new Set());
 
-    console.log('pressKeys',pressedKeys);
 
 
     const handleKeyUp = (ev: KeyboardEvent) => {
-        const { key } = ev;
-        
-        if (pressedKeys.includes(<KeyTypes>key)) {
-            setPressedKeys(pressedKeys.filter((value) => value !== key));
-        }
+        // console.log(ev.key);
+        pressedKeysRef.current.delete(ev.key);
     }
 
     const handleKeyDown = (ev: KeyboardEvent) => {
-        const { key } = ev;
-        // console.log(ev.key);
+        pressedKeysRef.current.add(ev.key);
 
-        if (key in KeyTypes) {
-            const mKey = key as KeyTypes;
-            setPressedKeys(Array.from(new Set([...pressedKeys, mKey])));
-        }
-
-        // 判断 pressedKeys 含有的元素与 targetKeys 相同
-        if (judgeSetEqual(new Set([...pressedKeys]), new Set([...targetKeys]))) {
+        if (judgeSetEqual(pressedKeysRef.current, new Set(targetKeys))) {
             setIsPressed(true);
         } else {
             setIsPressed(false);
         }
     }
 
-    
 
     useEffect(() => {
         document.addEventListener('keyup', handleKeyUp);
