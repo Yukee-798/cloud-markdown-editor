76afc9f7dd7fdbb933f27a766c7da89290fe633e
Yukee-798 <909397116@qq.com>
2021-04-13 08:35:27 +0800

    Update TabList

--- src/components/Right/TabList/tabList.tsx
+++ src/components/Right/TabList/tabList.tsx
@@ -10,7 +10,9 @@ interface ITabListProps extends IBaseProps {
     source?: ITab[];
     activedId?: string;
     unSavedFilesId?: string[];
-    onTabClick?: (id: string) => void;
+    onTabClick?: (key: string, event: MouseEvent) => void;
+    onTabRemove?: (key: string) => void;
+    onContentChange?: (value: string) => void;
 }
 
 
@@ -35,9 +37,28 @@ const TabList: React.FC<ITabListProps> = (props) => {
         source,
         activedId,
         unSavedFilesId,
-        onTabClick
+        onTabClick,
+        onTabRemove,
+        onContentChange
     } = props;
 
+
+
+
+    const handleEdit = (targetKey: any, action: 'remove' | 'add') => {
+
+        if (action === 'remove') {
+            onTabRemove?.(targetKey);
+        }
+
+
+    }
+
+
+    const onEditorChange = (value: string) => {
+
+        onContentChange?.(value)
+    }
 
     return (
         <div className='tabList-container'>
@@ -64,19 +85,18 @@ const TabList: React.FC<ITabListProps> = (props) => {
                             hideAdd
                             type="editable-card"
                             activeKey={activedId}
-                            // onTabClick={() => {
-                            //     onTabClick?.()
-                            // }}
-                            // onEdit={handleEdit}
+
+                            onTabClick={(key, event) => {
+                                onTabClick?.(key, event as any);
+                            }}
+                            onEdit={handleEdit}
                         >
-
-
                             {
                                 source?.map((tab: ITab) => (
                                     <Tabs.TabPane
                                         tab={tab.title}
                                         key={tab.id}
-                                        
+
                                         closeIcon={unSavedFilesId?.includes(tab.id) ? <FontAwesomeIcon icon={faCircle} /> : <span role="img" aria-label="close" className="anticon anticon-close"><svg viewBox="64 64 896 896" focusable="false" data-icon="close" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 00203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"></path></svg></span>}
                                     >
 
@@ -91,79 +111,12 @@ const TabList: React.FC<ITabListProps> = (props) => {
                                 spellChecker: false
                             }}
                             onChange={onEditorChange}
-                            value={activedTabInfo?.body}
+                            value={source?.find((item) => item.id === activedId)?.body}
                         />
                     </>
             }
         </div>
     )
-
-
-    // useEffect(() => {
-    //     if (isSaved) {
-    //         saveFile(activedId)
-    //     }
-    // }, [isSaved])
-
-
-
-
-    // useEffect(() => {
-    //     // openedFilesId 存放的顺序就是 tabList 渲染的顺序
-    //     const newTabList = openedFilesId.map((id) => {
-    //         // return fileList.find((file: IFile) => file.id === id);
-    //         return fileList[id]
-    //     });
-    //     setTabList(newTabList as IFile[])
-    // }, [openedFilesId, fileList]);
-
-    // useEffect(() => {
-    //     // setActivedTabInfo(fileList.find(file => file.id === activedId) as ITab);
-    //     setActivedTabInfo(fileList[activedId] as ITab);
-    // }, [activedId]);
-
-
-
-    // // const [hoverRef, isHoverd] = useHover();
-    // // const mHoverRef = hoverRef as any;
-    // // for (let i = 0; i < 10; i++) {
-    // //     const [hoverRef, isHoverd] = useHover();
-    // // }
-
-    // const handleEdit = (targetKey: any, action: 'remove' | 'add') => {
-
-    //     if (action === 'remove') {
-    //         // 如果移除的是当前激活的 tab，则将 activeId 切换到 openedFilesId 中当前关闭标签id 的前一个
-    //         closeTab(targetKey);
-    //         if (activedId === targetKey) {
-    //             // 如果当前关闭的 tab 处于 openedList 的第一个元素则 activedId 设置为其后面一个
-    //             if (targetKey === openedFilesId[0]) {
-    //                 updateActivedId(openedFilesId[1]);
-    //             } else {
-    //                 updateActivedId(openedFilesId[openedFilesId.findIndex(id => id === targetKey) - 1]);
-    //             }
-
-    //         }
-
-    //         // 如果当前移除的文件存在于未保存list中，则弹出 confirm
-    //         if (unSavedFilesId.includes(targetKey)) {
-    //             if (window.confirm(`Do you want to save the changes you made to ${activedTabInfo.title}?`)) {
-    //                 // saveFile({id: targetKey, newValue: })
-    //             }
-    //         }
-
-    //         setTabList(tabList.filter((tab: ITab) => !(tab.id === targetKey)));
-    //     }
-    // }
-
-    // const onEditorChange = (value: string) => {
-    //     // 如果文件的 body 发生了变化则将该文件状态设置为 unsaved
-    //     if (value !== activedTabInfo?.body) {
-    //         editFile({ id: activedId, newValue: value });
-    //     }
-    //     console.log(value);
-
-    // }
 }
 
 export default TabList;
--- src/components/Right/right.tsx
+++ src/components/Right/right.tsx
@@ -43,14 +43,49 @@ const Right: React.FC<IRightProps> = (props) => {
     } = props;
 
 
+
+    const handleTabRemove = (key: string) => {
+
+        /** 处理关闭激活的 tab 后，activedId 的变化 */
+        if (activedId === key) {
+            if (key === openedFilesId[0]) { // 激活 tab 为第一个 tab
+                updateActivedId(openedFilesId[1]);
+                closeTab(key);
+            } else { // 激活 tab 不为第一个 tab
+                updateActivedId(openedFilesId[openedFilesId.findIndex(id => id === key) - 1]);
+                closeTab(key)
+            }
+
+        }
+
+        /** 处理关闭的 tab 处于未保存状态的情况 */
+        if (unSavedFilesId.includes(key)) {
+            if (window.confirm(`Do you want to save the changes you made to ${fileList[key].title}?`)) {
+                // 保存文件到本地，并将 key 从 unSavedFilesId、openedFilesId 中移除，
+            }
+        }
+    }
+
+
+
     return (
         <div className='right-container'>
             <TabList
-                activedId={ }
-                source={ }
-                unSavedFilesId={}
-                onTabClick={() => {
+                activedId={activedId}
+                source={openedFilesId.map((id) => fileList[id])}
+                unSavedFilesId={unSavedFilesId}
+                onTabClick={(key, event) => {
+                    updateActivedId(key);
+                }}
 
+                onTabRemove={handleTabRemove}
+
+                onContentChange={(value) => {
+                    // 如果文件的 body 发生了变化则将该文件状态设置为 unsaved
+                    if (value !== fileList[activedId].body) {
+                        editFile({ id: activedId, newValue: value });
+                    }
+                    console.log(value);
                 }}
 
             />
